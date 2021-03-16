2a721b0f1b8ba7aaa883e137bf3fe6f638492311
aneetab <aneetab@gmail.com>
2021-03-16 14:29:46 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -32,25 +32,6 @@ function topFunction(){
     document.documentElement.scrollTop=0;
 }
  
-const navSlide=()=>{
-    const burger=document.querySelector('.burger');
-    const nav=document.querySelector('.nav-links');
-    const navLinks=document.querySelectorAll('.nav-links li');
-    burger.addEventListener('click',()=>{
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link,index)=>{
-            if(link.style.animation){
-                link.style.animation='';
-            }
-            else{
-                link.style.animation=`navLinkFade 0.5s ease forwards ${index/7 +0.5}s`;
-            }
-        });
-        burger.classList.toggle('toggle');
-});
-
-}
-navSlide();
 $(document).ready(function(){
     // banner image change
     let bannerCount = 0;
@@ -81,20 +62,6 @@ $(document).ready(function(){
         }
     }
 
-    // navigation bar toggle
-    $('#navbar-toggler').click(function(){
-        $('.navbar-collapse').slideToggle(600);
-    });
-
-    // fixed navbar 
-    $(window).scroll(function(){
-        let pos = $(window).scrollTop();
-        if(pos >= 800){
-            $('.navbar').addClass('fxd-navbar');
-        } else {
-            $('.navbar').removeClass('fxd-navbar');
-        }
-    });
 
     $('.subject_experts .owl-carousel').owlCarousel({
         loop: true,
